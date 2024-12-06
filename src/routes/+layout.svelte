efb27016e9c9d374f802c0d3e5e3f196bd1e957f
Blága Máté <blaga.mate05@gmail.com>
2024-12-06 15:36:26 +0100

    Add dayjs dependency and update layout with new styling
    - Added dayjs for date manipulation and relative time formatting.
    - Updated package.json and pnpm-lock.yaml to include dayjs.
    - Enhanced app.css with Google Fonts and applied styles to root and body.
    - Modified +layout.svelte to include a header and main structure.
    - Replaced default content in +page.svelte with achievements and job experiences.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,23 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="min-h-full bg-stone-900 text-stone-300">
+	<div class="container ml-auto mr-auto max-w-5xl">
+		<header>
+			<nav class="flex items-center justify-between md:px-16 p-6">
+				<div class="flex gap-2">
+					<span class="text-2xl font-bold">Tamás Vince</span>
+					<span class="text-2xl font-light text-stone-700">tvk.lol</span>
+				</div>
+				<ul class="flex gap-3 text-lg">
+					<li>Link</li>
+					<li>Link</li>
+					<li>Link</li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			{@render children()}
+		</main>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,92 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import dayjs from "dayjs";
+    import relativeTime from "dayjs/plugin/relativeTime";
+
+    dayjs.extend(relativeTime);
+
+    const results = [
+        {
+            name: "LEGO WRO Hungary",
+            date: new Date("2023-06-30"),
+            team: true,
+            place: 7
+        },
+        {
+            name: "Szeresd a Pingvint 2023",
+            date: new Date("2023-12-06"),
+            team: false,
+            place: 1
+        },
+        {
+            name: "JuniorSkills rendszerüzemeltető",
+            date: new Date("2024-04-06"),
+            team: false,
+            place: 3
+        },
+        {
+            name: "Nokia 2024 Tavaszi Hackathon",
+            date: new Date("2024-07-01"),
+            team: false,
+            place: null
+        },
+        {
+            name: "Szeresd a Pingvint 2024",
+            date: new Date("2024-12-06"),
+            team: false,
+            place: 1
+        }
+    ]
+
+    const jobs = [
+        {
+            name: "Nokia Solutions and Networks",
+            position: "Software Developer Intern",
+            date: {
+                start: new Date("2023-06-01"),
+                end: new Date("2023-08-31")
+            },
+            description: "I worked on internal tools using Python and the Robot Framework. Read my performance review <a class='text-blue-500 underline' href='#'>here</a>."
+        }
+    ]
+</script>
+
+<div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 md:p-8">
+        <div>
+            <h1 class="font-semibold text-2xl mb-4 flex justify-between">
+                <span>What I've achieved</span>
+                <span class="text-right font-light text-stone-500 text-xl">{results.length} results</span>
+            </h1>
+            <ul class="list-disc">
+                {#each results as result}
+                    <li class="flex justify-between mb-4">
+                        <div class="">
+                            <h2 class="text-xl font-bold">{result.name}</h2>
+                            <div class="flex gap-3">
+                                <p class="text-stone-400">{result.team ? "Team" : "Individual"}</p>
+                                <span class="text-stone-500">|</span>
+                                <p class="text-stone-400">{dayjs(result.date).fromNow()}</p>
+                            </div>
+                        </div>
+                        <p class="text-emerald-500 font-bold text-xl">{result.place ? `${result.place}. place` : "Qualified"}</p>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+        <div>
+            <h1 class="font-semibold text-2xl mb-8">Where I worked</h1>
+            <ul class="">
+                {#each jobs as job}
+                    <li class="mb-8">
+                        <h2 class="text-xl font-bold text-blue-500">{job.name}</h2>
+                        <p class="text-stone-400 font-semibold">{job.position}</p>
+                        <div class="flex gap-3">
+                            <p class="text-stone-500">{dayjs(job.date.start).format("MMMM YYYY")} - {dayjs(job.date.end).format("MMMM YYYY")}</p>
+                        </div>
+                        <p>{@html job.description}</p>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    </div>
+</div>
